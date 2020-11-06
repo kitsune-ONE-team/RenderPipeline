8aabe0a3565856f855692569d34d9c002f0e29c3
Yonnji <yonnji@kitsune.one>
2020-11-06 13:25:39 +0300

    version up

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
 
     setuptools.setup(
         name='render_pipeline',
-        version='2.0.0',
+        version='2.0.1',
         description='RenderPipeline',
         long_description=(
             'Physically Based Shading and Deferred Rendering '
